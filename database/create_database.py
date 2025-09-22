2b3009009fca023dfbca39e5d910317dd64ef333
İpek Öner <ipekoner2006@gmail.com>
2025-09-22 17:34:27 +0200

    Update create_database.py

--- database/create_database.py
+++ database/create_database.py
@@ -23,18 +23,44 @@ cursor.execute("""
         surname TEXT,
         constructor_name TEXT,
         constructor_points INT,
-        podium INT
+        podium INT,
+        driverId_encoded INT,
+        constructorId_encoded INT,
+        GP_name_encoded INT
+               
     );    """)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS drivers(
+               driverId_encoded INT PRIMARY KEY,
+               driver_name TEXT UNIQUE); """)
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS constructors(
+               constructorId_encoded INT PRIMARY KEY,
+               constructor_name TEXT UNIQUE); """)
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tracks(
+               GP_name_encoded INT PRIMARY KEY,
+               GP_track_name TEXT UNIQUE); """)
+
 
 conn.commit()
 
-drivers = f1_info[["forename," "surname"]].drop_duplicates()
-drivers.columns = ["driver_names"]
+drivers = f1_info[["forename", "surname"]].drop_duplicates()
+drivers["driver_names"] = drivers["forename"] + " " + drivers["surname"]
+drivers = drivers[["driver_name"]].reset_index(drop=True)
+drivers["driverId_encoded"] = drivers.index
 
-constructors = f1_info[["constructor_name"]].drop_duplicates()
-constructors.columns = ["constructor_names"]
+drivers.to_sql("drivers", conn, if_exists ="append", index=False)
 
-tracks = f1_info[["name"]].drop_duplicates()
-tracks.columns = ["GP_track_names"]
+
+constructors = f1_info[["constructor_name"]].drop_duplicates().reset_index(drop=True)
+constructors["constructorId_encoded"] = constructors.index
+constructors.to_sql("constructors", conn, if_exists ="append", index=False)
+
+tracks = f1_info[["name"]].drop_duplicates().reset_index(drop=True)
+tracks = tracks.rename(columns={"name": "GP_track_name"})
+tracks["GP_name_encoded"] = tracks.index
+tracks.to_sql("tracks", conn, if_exists="replace", index=False)
 
 f1_info.to_sql("f1_merged", conn, if_exists = "append", index = False)
