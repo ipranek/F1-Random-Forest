d68e3cc2502210e40b0ae2a35e5da8a1f43831c7
İpek Öner <ipekoner2006@gmail.com>
2025-09-25 16:08:20 +0200

    fixes

--- app.py
+++ app.py
@@ -14,21 +14,28 @@ app = Flask(__name__)
 @app.route("/", methods = ["GET"])
 def home():
     drivers = get_drivers()
-    constructors = get_constructors()
-    tracks = get_tracks()
+    constructors = get_constructor()
+    tracks = get_track()
     return render_template("home.html", drivers = drivers, constructors = constructors, tracks=tracks)
 def get_drivers():
     connection_db = sqlite3.connect(DB_PATH)
-    query = "SELECT driverId_encoded from drivers WHERE driver_names" #later go and change the sql table and include the driver_fullname, no need for the concetenation in the sql
+    query = "SELECT driverId_encoded, driver_fullname FROM drivers" #later go and change the sql table and include the driver_fullname, no need for the concetenation in the sql
     drivers = pd.read_sql(query, connection_db)
     connection_db.close()
     return drivers.to_dict(orient = "records")
 def get_constructor():
     connection_db = sqlite3.connect(DB_PATH)
-    query = "SELECT constructorId_encoded from constructors WHERE constructor_names" #lowkey check var name, names or name
-    constructors = pd.read_Sql(query, connection_db)
+    query = "SELECT constructorId_encoded, constructor_names FROM constructors" #lowkey check var name, names or name
+    constructors = pd.read_sql(query, connection_db)
     connection_db.close
-if __name__ == "main":
+    return constructors.to_dict(orient = "records")
+def get_track():
+    connection_db = sqlite3.connect(DB_PATH)
+    query = "SELECT GP_name_encoded, GP_track_name FROM tracks"
+    tracks = pd.read_sql(query, connection_db)
+    connection_db.close()
+    return tracks.to_dict(orient = "records")
+if __name__ == "__main__":
     app.run(debug=True, port=5001)
 
 
